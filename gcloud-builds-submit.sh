2871707995624cf0833250fda58c26dd5f10cc05
m-oka-system <m-oka-system@outlook.com>
2021-11-19 22:08:51 +0900

    プロジェクトIDの指定を追加

--- gcloud-builds-submit.sh
+++ gcloud-builds-submit.sh
@@ -3,4 +3,4 @@
 PROJECT_ID="k8s-dev-328907"
 TAG_NAME=$(date '+%Y-%m-%d_%H-%M-%S')
 
-gcloud builds submit --tag gcr.io/$PROJECT_ID/scaffold-ruby:latest .
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/scaffold-ruby:${TAG_NAME} --project $PROJECT_ID .
