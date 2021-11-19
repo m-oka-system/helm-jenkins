96a03bc8396e87098cfb6d2a91b672b3279223fd
m-oka-system <m-oka-system@outlook.com>
2021-11-19 22:36:14 +0900

    サービスアカウントの認証を追加

--- gcloud-builds-submit.sh
+++ gcloud-builds-submit.sh
@@ -3,4 +3,5 @@
 PROJECT_ID="k8s-dev-328907"
 TAG_NAME=$(date '+%Y-%m-%d_%H-%M-%S')
 
+gcloud auth activate-service-account --key-file /var/secrets/google/key.json
 gcloud builds submit --tag gcr.io/${PROJECT_ID}/scaffold-ruby:${TAG_NAME} --project $PROJECT_ID .
