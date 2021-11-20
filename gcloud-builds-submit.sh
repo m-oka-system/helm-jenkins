bb36777ce872bd90edefbf6d2b3cb40ce6bce5fa
m-oka-system <m-oka-system@outlook.com>
2021-11-20 16:18:21 +0900

    タイムアウトを追加

--- gcloud-builds-submit.sh
+++ gcloud-builds-submit.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 PROJECT_ID="k8s-dev-328907"
 TAG_NAME=$(date '+%Y-%m-%d_%H-%M-%S')
 
 gcloud auth activate-service-account --key-file /var/secrets/google/key.json
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/scaffold-ruby:${TAG_NAME} --project $PROJECT_ID .
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/scaffold-ruby:${TAG_NAME} --project $PROJECT_ID --timeout 1s .
+
+echo "=== END ==="
